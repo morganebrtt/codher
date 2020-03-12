87114a8fd0c4fa6b2798c8c595ca33c634c6e761
Morgane Broutet <morgane.broutet@gmail.com>
2020-03-12 13:00:17 +0100

    add server dependancies

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+// Dépendances
+const express = require('express');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken'),
+    app = express();
+
