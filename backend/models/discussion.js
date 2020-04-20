205438b445f8ea4532df243ffa539c2e99fdae2a
Morgane Broutet <morgane.broutet@gmail.com>
2020-04-20 12:28:48 +0200

    add discussion model

---
+++ backend/models/discussion.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+let discussionSchema = new.mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    likedUserId : {
+        type : mongoose.Schema.Types.ObjectId, ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Discussion', discussionSchema);
