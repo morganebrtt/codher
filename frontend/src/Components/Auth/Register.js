df179e9220727776ef2212a50aa36be0cad13114
Morgane Broutet <morgane.broutet@gmail.com>
2020-04-26 16:25:40 +0200

    add fetch in Register Component

--- frontend/src/Components/Auth/Register.js
+++ frontend/src/Components/Auth/Register.js
@@ -4,9 +4,10 @@ import ContainedButton from "../Buttons/ContainedButton";
 import OutlinedButton from "../Buttons/OutlinedButton";
 
 const Register = () => {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
   const handleChange = (event) => {
     if (event.currentTarget.name === "email") {
@@ -22,20 +23,43 @@ const Register = () => {
     event.preventDefault();
     console.log(event.currentTarget);
     setIsLoading(true);
-    setIsLoading(false);
+    fetch("http://localhost:3050/auth/register", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({email, password}),
+    })
+    .then ((res) => {
+      console.log(res);
+      if (res.status === 200 && res.ok === true) {
+        setMessage("Inscription réussie!");
+      } else {
+        setMessage("une erreur s'est produite");
+      }
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      setMessage("une erreur s'est produite");
+      console.log(error);
+      setIsLoading(false);
+    });
   };
 
   return (
     <div id="loginContent">
       <form onSubmit={handleSumbit}>
         {isLoading && <p>Loading</p>}
+        {message && <p>{message}</p>}
         <input 
-        type="email"
+         required
+         type="email"
          name="email" 
          value={email}
          onChange={handleChange}
          placeholder="Votre email" />
         <input
+          required
           type="password"
           name="password"
           value={password}
