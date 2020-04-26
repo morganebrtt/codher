8b509ed20ead3b0543953950722e210c25707b58
Morgane Broutet <morgane.broutet@gmail.com>
2020-04-26 17:25:43 +0200

    add CompleteRegistration component

---
+++ frontend/src/Components/CompleteRegistration/CompleteRegistration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CompleteRegistration.css';
+
+const CompleteRegistration = () => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default CompleteRegistration; 
